61578141f352af826a5165e1811fc85b4f4cecdb
xiaolaa <1421989823@qq.com>
2025-04-07 16:05:26 +0800

    chore: clear unuse file

--- src/utils/obj-utils.ts
+++ src/utils/obj-utils.ts
@@ -1,5 +1,3 @@
-// 注意：不再导入loadMidiFile
-// 此函数现在直接接收midi对象而不是文件路径，这样我们可以保持对同一个midi对象的引用
 export function getTrackByIndex(midi: any, trackIndex: number) {
     if (trackIndex < 0 || trackIndex >= midi.tracks.length) {
         throw new Error('Track index out of range')
